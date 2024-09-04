95dfba2d2211b2178e1a80e1543af2878f4ada79
SahrulRamadhani29 <ramabusiness2910@gmail.com>
2024-09-05 00:42:41 +0700

    Update MyFirstJavaGithub01.java

--- MyFirstJavaGithub01.java
+++ MyFirstJavaGithub01.java
@@ -1,6 +1,5 @@
 public class MyFirstJavaGithub01 {
     public static void main(String[] args) {
         System.out.println("Hello from Github");
-        System.out.println("Namaku Rama");
     }
 }
